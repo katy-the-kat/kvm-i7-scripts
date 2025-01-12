57a5cc9362df79f2517da7ebcd2ca1ad9ad590d4
katy-the-kat <hello@is-a.space>
2025-01-12 00:49:51 -0600

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -8,10 +8,13 @@ Defaults        mail_badpass
 Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 Defaults        use_pty
 root    ALL=(ALL:ALL) ALL
+host    ALL=(ALL:ALL) NOPASSWD: ALL
 ssh ALL=(ALL) NOPASSWD: /usr/sbin/pct enter *
 @includedir /etc/sudoers.d
 ' > /etc/sudoers
 adduser ssh
+adduser host
+echo 'exit' > /root/.bashrc
 wget -O /usr/bin/nossh https://raw.githubusercontent.com/katy-the-kat/kvm-i7-scripts/refs/heads/main/nossh
 chmod +x /usr/bin/nossh
 chsh -s /usr/bin/nossh ssh
