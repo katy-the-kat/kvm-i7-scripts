0dc2ee935835be9479a269d4d70cdd95c635db31
Katy <hello@is-a.space>
2025-02-03 12:33:49 -0600

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -23,14 +23,18 @@ docker build -t utmp .
 docker network create --subnet=10.73.17.0/24 kvmnet
 
 cat <<EOF > Dockerfile
-FROM ubuntu:22.04
-
-#RUN sed -i -e 's|http://[^ ]*archive.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' -e 's|http://[^ ]*security.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list  
-RUN apt-get update 
-RUN yes | apt-get install -y nano htop wget dialog openssh-server openssh-client sudo snapd curl iputils-ping
-RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-ENTRYPOINT ["/sbin/init"]
+FROM ubuntu:22.04  
+RUN sed -i -e 's|http://[^ ]*archive.ubuntu.com/ubuntu|http://it.archive.ubuntu.com/ubuntu|g' -e 's|http://[^ ]*security.ubuntu.com/ubuntu|http://it.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list  
+RUN apt update
+RUN apt install -y openssh-server software-properties-common curl sudo wget dialog
+RUN add-apt-repository ppa:zhangsongcui3371/fastfetch -y  
+RUN apt update
+RUN apt install -y fastfetch  
+RUN wget -O /usr/bin/neofetch https://github.com/dylanaraps/neofetch/raw/master/neofetch  
+RUN chmod +x /usr/bin/neofetch  
+RUN mkdir /var/run/sshd    
+RUN echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config  
+CMD ["/usr/sbin/sshd", "-D"]
 EOF
 
 echo "fs.inotify.max_user_instances = 2147483647" > /etc/sysctl.conf 
