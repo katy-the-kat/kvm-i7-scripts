186f495918f9050b9ffb7a36dd7174ab14a24b76
Katy <hello@is-a.space>
2025-02-03 12:32:35 -0600

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -9,8 +9,6 @@ FROM ubuntu:22.04
 RUN apt-get update 
 RUN yes | apt-get install -y nano htop wget dialog openssh-server openssh-client sudo tmate snapd curl iputils-ping
 RUN yes | unminimize
-RUN wget -O /usr/bin/port https://raw.githubusercontent.com/katy-the-kat/realinstallscript/refs/heads/main/ipv4x
-RUN chmod +x /usr/bin/port
 RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 RUN echo 'root:root' | chpasswd
 RUN printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
@@ -23,6 +21,17 @@ EOF
 docker build -t utmp .
 
 docker network create --subnet=10.73.17.0/24 kvmnet
+
+cat <<EOF > Dockerfile
+FROM ubuntu:22.04
+
+#RUN sed -i -e 's|http://[^ ]*archive.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' -e 's|http://[^ ]*security.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list  
+RUN apt-get update 
+RUN yes | apt-get install -y nano htop wget dialog openssh-server openssh-client sudo snapd curl iputils-ping
+RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+ENTRYPOINT ["/sbin/init"]
+EOF
 
 echo "fs.inotify.max_user_instances = 2147483647" > /etc/sysctl.conf 
 sysctl -p 
